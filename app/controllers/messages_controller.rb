6cdb25ea6792c791caacd7652ff252e8c1f1d2a4
shoiraTa <shoira.shakirovna@gmail.com>
2022-06-21 16:57:52 +0400

    render message immidiately in views

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
 
   # GET /messages or /messages.json
   def index
-    @messages = Message.all
+    @messages = Message.order(created_at: :desc)
   end
 
   # GET /messages/1 or /messages/1.json
@@ -26,7 +26,8 @@ class MessagesController < ApplicationController
     respond_to do |format|
       format.turbo_stream do 
         render turbo_stream: [
-          turbo_stream.update('new_message', partial:'messages/form', locals: {message: Message.new})
+          turbo_stream.update('new_message', partial:'messages/form', locals: {message: Message.new}),
+          turbo_stream.prepend('messages', partial:'messages/message', locals: {message: @message})
         ]
       end
 
